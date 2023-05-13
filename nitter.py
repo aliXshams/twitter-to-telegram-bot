f23820040d96b3be8815b5f1668da11cb99e904f
Ali Shams <aliXshams@proton.me>
2023-05-13 12:41:00 +0330

    Remove commented-out code

--- nitter.py
+++ nitter.py
@@ -1,14 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-
-# headers = {
-#             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-#         }
  
 class ReadRss:
- 
     def __init__(self, rss_url, headers):
- 
         self.url = rss_url
         self.headers = headers
         try:
@@ -24,9 +18,3 @@ class ReadRss:
             print(e)
         self.tweets = self.soup.findAll('item')
         self.tweets_dicts = [{'title':a.find('title').text, 'creator':a.find('dc:creator').text,'description':a.find('description').text,'pub_date':a.find('pubDate').text} for a in self.tweets]
- 
-# if __name__ == '__main__':
-#     query = "%23cybersecurity%20OR%20%23zeroday"
-#     url = f"https://nitter.net/search/rss?f=tweets&q={query}&since=&until=&near="
-#     feed = ReadRss(url, headers)
-#     print(feed.tweets_dicts[0]['description'])
